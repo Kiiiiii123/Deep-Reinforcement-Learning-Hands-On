83ffccba3f0b9a050d175537fec3898e5248a11b
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-18 00:09:49 +0800

    Update 02_tensorboard.py

--- Chapter03/02_tensorboard.py
+++ Chapter03/02_tensorboard.py
@@ -5,7 +5,7 @@ from tensorboardX import SummaryWriter
 if __name__ == '__main__':
     writer = SummaryWriter()
 
-    funcs = {"sin":math.sin, "cos":math.cos, "tan":math.tan}
+    funcs = {"sin": math.sin, "cos": math.cos, "tan": math.tan}
 
     for angle in range(-360, 360):
         angle_rad = angle * math.pi / 180
