c88d8b032bb62ddf0996dc054944363ec1904540
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-18 21:34:54 +0800

    Update 02_frozenlake_naive.py

--- Chapter04/02_frozenlake_naive.py
+++ Chapter04/02_frozenlake_naive.py
@@ -87,7 +87,7 @@ def filter_batch(batch, percentile):
 
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0")
+    env = DiscreteONeHotWrapper(gym.make("FrozenLake-v0"))
     # env = gym.wrappers.Monitor(env, directory="mon", force=True)
     obs_size = env.observation_space.shape[0]
     n_actions = env.action_space.n
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
     objective = nn.CrossEntropyLoss()
     optimizer = optim.Adam(params=net.parameters(), lr=0.01)
-    writer = SummaryWriter(comment="-cartpole")
+    writer = SummaryWriter(comment="-frozenlake-naive")
 
     for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
         obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
