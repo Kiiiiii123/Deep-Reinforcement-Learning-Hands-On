a96a4f506a606d6e6b53ba9dd492509e5a7bacfd
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-18 14:37:04 +0800

    Update 01_cartpole.py

--- Chapter04/01_cartpole.py
+++ Chapter04/01_cartpole.py
@@ -26,6 +26,39 @@ class Net(nn.Module):
 
 
 # 存储python对象序列，两大参数：tuple名字和其中域的名字
+# 总奖励＋总步数
 Episode = namedtuple('Episode', field_names=['reward', 'steps'])
+# 单步的观察值＋选择动作
 EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
 
+
+def iterate_batches(env, net, batch_size):
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs = env.reset()
+    sm = nn.Softmax(dim=1)
+    while True:
+        obs_v = torch.FloatTensor([obs])
+        act_probs_v = sm(net(obs_v))
+        act_probs = act_probs_v.data.numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, is_done, _ = env.step(action)
+        episode_reward += reward
+        # 加入这一步
+        episode_steps.append(EpisodeStep(observation=obs, action=action))
+        if is_done:
+            # 加入这个episode
+            batch.append(Episode(reward=episode_reward, steps=episode_steps))
+            # 重置
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs = env.reset()
+            if len(batch) == batch_size:
+                # 迭代器
+                yield batch
+                batch = []
+            obs = next_obs
+
+
+def filter_batch()
