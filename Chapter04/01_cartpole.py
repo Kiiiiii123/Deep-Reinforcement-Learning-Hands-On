d54ac17000aae88223b419545cd1df59d74de391
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-18 16:07:13 +0800

    Update 01_cartpole.py

--- Chapter04/01_cartpole.py
+++ Chapter04/01_cartpole.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
         loss_v.backward()
         optimizer.step()
         print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f"%(iter_no, loss_v.item(), reward_m, reward_b))
-        writer.add_scalar("loss", loss_v.item(),iter_no)
+        writer.add_scalar("loss", loss_v.item(), iter_no)
         writer.add_scalar("reward_bound", reward_b, iter_no)
         writer.add_scalar("reward_mean", reward_m, iter_no)
         if reward_m > 199:
