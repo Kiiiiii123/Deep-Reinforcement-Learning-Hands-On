669ff21c6d1a0889ff408808287df901fa22dfd8
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-18 13:32:24 +0800

    Update 01_cartpole.py

---
+++ Chapter04/01_cartpole.py
@@ -0,0 +1,27 @@
+import gym
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 16
+PERCENTILE = 70
+
+
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
