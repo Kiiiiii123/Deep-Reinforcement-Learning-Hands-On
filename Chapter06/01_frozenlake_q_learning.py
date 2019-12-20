4e020a3b2b336b4524ca1817a5d65ced20d03ec8
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-20 21:12:58 +0800

    Update 01_frozenlake_q_learning.py

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -25,7 +25,7 @@ class Agent:
     def best_value_and_action(self, state):
         best_value, best_action = None, None
         for action in range(self.env.action_space.n):
-            action_value = self.values.get([state, action])
+            action_value = self.values[(state, action)]
             if best_value is None or action_value > best_value:
                 best_value = action_value
                 best_action = action
@@ -40,16 +40,41 @@ class Agent:
 
     # 实战
     def play_episode(self, env):
-         total_reward = 0.0
-         state = self.env.reset()
-         while True:
-             _, action = self.best_value_and_action(state)
-             new_state, reward, is_done, _ = self.env.step(action)
-             total_reward += reward
-             if is_done:
-                 break
-             state = new_state
-         return total_reward
+        total_reward = 0.0
+        state = self.env.reset()
+        while True:
+            _, action = self.best_value_and_action(state)
+            new_state, reward, is_done, _ = self.env.step(action)
+            total_reward += reward
+            if is_done:
+                break
+            state = new_state
+        return total_reward
+
 
 if __name__ == "__main__":
+    test_env = gym.make(ENV_NAME)
+    agent = Agent()
+    writer = SummaryWriter(comment="-q-learning")
+
+    ite_no = 0
+    best_reward = 0.0
+    while True:
+        ite_no += 1
+        s, a, r, next_s = agent.sample_env()
+        agent.value_update(s, a, r, next_s)
+
+        reward = 0.0
+        for _ in range(TEST_SPISODES):
+            reward += agent.play_episode(test_env)
+        avg_reward = reward / TEST_SPISODES
+        writer.add_scalar("avg_reward", avg_reward, ite_no)
+        if avg_reward > best_reward:
+            print("Best reward updated %.3f -> %.3f" % (best_reward, avg_reward))
+            best_reward = reward
+        if best_reward > 0.80:
+            print("Solved in %d iterations!" % ite_no)
+            break
+    writer.close()
+
 
