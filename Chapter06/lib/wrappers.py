83d742335bb779c713348acea401f84ba1e95efd
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-21 15:52:27 +0800

    Update wrappers.py

--- Chapter06/lib/wrappers.py
+++ Chapter06/lib/wrappers.py
@@ -78,3 +78,26 @@ class ProcessFrame84(gym.ObservationWrapper):
         x_t = resized_screen[18:102, :]
         x_t = np.reshape(x_t, [84, 84, 1])
         return x_t.astype(np.uint8)
+
+
+# 将连续帧堆在一起作为observation
+class BufferWrapper(gym.ObservationWrapper):
+    def __init__(self, env, n_steps, dtype=np.float32):
+        super(BufferWrapper, self).__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis=0), old_space.high.repeat(n_steps, axis=0), dtype=dtype)
+
+    def reset(self):
+        self.buffer = np.zeros_like(self.observation_space.low, dtype=self.dtype)
+        return self.observation(self.env.reset())
+
+    # Buffer中observation的更迭
+    def observation(self, observation):
+        self.buffer[:,-1] = self.buffer[1:]
+        self.buffer[-1] = observation
+        return self.buffer
+
+
+class ImageToPytorch(gym.ObservationWrapper):
+
