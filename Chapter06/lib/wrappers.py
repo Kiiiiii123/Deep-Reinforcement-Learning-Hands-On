0bc94eaa092a5f2ee4bd64c7566226bbc54df972
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-21 14:59:50 +0800

    Update wrappers.py

--- Chapter06/lib/wrappers.py
+++ Chapter06/lib/wrappers.py
@@ -54,5 +54,27 @@ class MaxAndSkipEnv(gym.Wrapper):
         return obs
 
 
-class
+# 将210*160*3缩放到84*84*1
+class ProcessFrame84(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.unit8)
 
+    def observation(self, obs):
+        return ProcessFrame84.process(obs)
+
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
+        else:
+            assert False, "Unknown resolution"
+        # 色度变换
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        # 使用像素区域关系进行重采样
+        resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
