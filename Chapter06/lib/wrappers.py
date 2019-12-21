d55c0e0b9fa920a457a1dbb1a4cf2999cad0cada
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-21 14:00:12 +0800

    Update wrappers.py

---
+++ Chapter06/lib/wrappers.py
@@ -0,0 +1,42 @@
+import cv2
+import gym
+import gym.spaces
+import numpy as np
+import collections
+
+
+# 需要在游戏开始时按下FIRE按钮的，校验游戏中的边角细节
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env=None):
+        super(FireResetEnv, self).__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def step(self, action):
+        return self.env.step.action()
+
+    def reset(self):
+        self.env.reset()
+        obs, _, done, _ =self.env.step(1)
+        if done:
+            self.env.reset()
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset()
+        return obs
+
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only every 'skip-th' frame"""
+        super(MaxAndSkipEnv, self).__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = collections.deque(maxlen=2)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+
+
