6315a90515841a2fbe6b5f4c40f529783b925c4a
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-23 17:03:47 +0800

    Update common.py

--- Chapter07/lib/common.py
+++ Chapter07/lib/common.py
@@ -23,6 +23,7 @@ HYPERPARAMS = {
 }
 
 
+# 将样本数据解包到numpy数组
 def unpack_batch(batch):
     states, actions, rewards, dones, last_states = [], [], [], [], [], []
     for exp in batch:
@@ -36,3 +37,76 @@ def unpack_batch(batch):
         else:
             last_states.append(np.array(exp.last_state, copy=False))
     return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
+
+
+# 计算损失函数
+def calc_loss_dqn(batch, net, tgt_net, gamma, device="cpu"):
+    states, actions, rewards, dones, next_states = unpack_batch(batch)
+
+    # 转换为张量
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actiosn_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.ByteTensor(dones).to(device)
+
+    # 计算预测值和目标值
+    state_action_values = net(states_v).gather(1, actiosn_v.unsqueeze(-1)).squeeze(-1)
+    next_state_values = tgt_net(next_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+
+    # 计算损失值
+    expected_state_action_values = next_state_values.detach() * gamma +rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
+# 介绍两个简化训练循环的工具
+# 执行epsilon的线性衰减策略
+class EpsilonTracker:
+    def __init__(self, epsilon_greedy_selector, params):
+        self.epsilon_greedy_selector = epsilon_greedy_selector
+        self.epsilon_start = params['epsilon_start']
+        self.epsilon_final = params['epsilon_final']
+        self. epsilon_frames = params['epsilon_frames']
+        self.frame(0)
+
+    def frame(self, frame):
+        self.epsilon_greedy_selector.epsilon = max(self.epsilon_final, self.epsilon_start - frame / self.epsilon_frames)
+
+
+# 获取每个episode结束时的奖励，以及最近episodes的平均奖励
+class RewardTracker:
+    def __init__(self, writer, stop_reward):
+        self.writer = writer
+        self.stop_reward = stop_reward
+
+    def __enter__(self):
+        self.ts = time.time()
+        self.ts_frame = 0
+        self.total_rewards = []
+        return self
+
+    def __exit__(self, *args):
+        self.writer.close()
+
+    def reward(self, reward, frame, epsilon=None):
+        self.total_rewards.append(reward)
+        speed = (frame - self.ts_frame) / (time.time() - self.ts)
+        self.ts_frame = frame
+        self.ts = time.time()
+        mean_reward = np.mean(self.total_rewards[-100:])
+        epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon
+        print("%d: done %d games, mean reward %.3f, speed %.2f f/s%s" % (frame, len(self.total_rewards), mean_reward, speed, epsilon_str))
+        sys.stdout.flush()
+        if epsilon is not None:
+            self.writer.add_scalar("epsilon", epsilon, frame)
+            self.writer.add_scalar("speed", speed, frame)
+            self.writer.add_scalar("reward_100", mean_reward, frame)
+            self.writer.add_scalar("reward", reward, frame)
+            if mean_reward > self.stop_reward:
+                print("Solved in %d frames!" % frame)
+                return True
+            return False
+
+
+
