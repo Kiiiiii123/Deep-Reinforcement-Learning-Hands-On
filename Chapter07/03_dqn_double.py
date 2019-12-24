51ee1c0e01532e0ac915cab1f544779636e0943d
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-25 00:31:53 +0800

    Update 03_dqn_double.py

--- Chapter07/03_dqn_double.py
+++ Chapter07/03_dqn_double.py
@@ -14,6 +14,7 @@ from lib import dqn_model, common
 STATES_TO_EVALUATE = 1000
 EVAL_EVERY_FRAME = 100
 
+
 def calc_loss(batch, net, tgt_net, gamma, device="cpu", double=True):
     states, actions, rewards, dones, next_states = common.unpack_batch(batch)
 
@@ -28,5 +29,50 @@ def calc_loss(batch, net, tgt_net, gamma, device="cpu", double=True):
         # 主网络为下一状态选择最佳动作
         next_state_actions = net(next_states_v).max(1)[1]
         # 目标网络根据主网络选择的动作提取下一状态的值
-        next_state_values =
+        next_state_values = tgt_net(next_states_v).gather(1, next_state_actions.unsqueeze(-1)).squeeze(-1)
+    else:
+        next_state_values = tgt_net(next_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+
+    expected_state_action_values = next_state_values.detach() * gamma + rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+
+# 取样1000 states计算状态值用于比较是否解决过估计问题
+def calc_values_of_states(states, net, device="cpu"):
+    mean_vals = []
+    for batch in np.array_split(states, 64):
+        states_v = torch.tensor(batch).to(device)
+        action_values_v = net(states_v)
+        best_action_values_v = action_values_v.max(1)[1]
+        mean_vals.append(best_action_values_v.mean().item())
+    return np.mean(mean_vals)
+
+
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    # 新加命令行命令，启动或关闭double DQN
+    parser.add_argument("--double", default=False, action="store_true", help="Enable double DQN")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-double=" + str(args.double))
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+
+    tgt_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(net, selector, device=device)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    frame_idx = 0
+    eval_states = None
 
