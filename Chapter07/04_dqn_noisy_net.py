ff2bfeb2e644808240c9180c699ad6b1078f8762
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-25 23:17:12 +0800

    Update Chapter07/lib/04_dqn_noisy_net.py

--- Chapter07/04_dqn_noisy_net.py
+++ Chapter07/04_dqn_noisy_net.py
@@ -30,8 +30,8 @@ class NoisyDQN(nn.Module):
         conv_out_size = self._get_conv_out(input_shape)
         # 线性层加入权重噪声
         self.noisy_layers = [
-            model.NoisyLinear(conv_out_size, 512),
-            model.NoisyLinear(512, n_actions)
+            dqn_model.NoisyLinear(conv_out_size, 512),
+            dqn_model.NoisyLinear(512, n_actions)
         ]
         self.fc = nn.Sequential(
             # 列表中的第一个
@@ -51,5 +51,59 @@ class NoisyDQN(nn.Module):
         conv_out = self.conv(fx).view(fx.size()[0], -1)
         return self.fc(conv_out)
 
+    # 计算噪声层的信噪比用于监督SNR
     def noisy_layers_sigma_snr(self):
-        return
+        return [
+            ((layer.weight ** 2).mean().sqrt() / (layer.sigma_weight ** 2).mean().sqrt()).data.cpu().numpy()[0] for layer in self.noisy_layers
+        ]
+
+
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-noisy-net")
+    net = NoisyDQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = ptan.agent.TargetNet(net)
+    agent = ptan.agent.DQNAgent(net, ptan.actions.ArgmaxActionSelector(), device=device)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    frame_idx = 0
+
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx):
+                    break
+
+            if len(buffer) < params['replay_initial']:
+                continue
+
+            optimizer.zero_grad()
+            batch = buffer.sample(params['batch_size'])
+            loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], device=device)
+            loss_v.backward()
+            optimizer.step()
+
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
+
+            # 每过500帧查询一次信噪比
+            if frame_idx % 500 == 0:
+               snr_vals = net.noisy_layers_sigma_snr()
+               # 共两个噪声层，因此需要两张检测表
+               for layer_idx, sigma_l2 in enumerate(snr_vals):
+                   writer.add_scalar("sigma_snr_layer_%d" % (layer_idx+1), sigma_l2, frame_idx)
