e7e63155eab1e819b75db993914f1e43798032c6
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-26 15:51:11 +0800

    Update Chapter07/05_dqn_prio_replay.py

--- Chapter07/05_dqn_prio_replay.py
+++ Chapter07/05_dqn_prio_replay.py
@@ -30,7 +30,7 @@ class PrioReplayBuffer:
         def __len__(self):
             return len(self.buffer)
 
-        # 存储样本进Ｂbuffer时便需要考虑优先度
+        # 存储样本进buffer时便需要考虑优先度
         def populate(self, count):
             max_prio = self.priorities.max() if self.buffer else 1.0
             for _ in range(count):
@@ -66,15 +66,85 @@ class PrioReplayBuffer:
             # 索引用于为被采样的样本更新优先度
             return samples, indices, weights
 
-        # 更新优先度
+        # 根据计算获得的loss更新优先度
         def update_priorities(self, batch_indices, batch_priorities):
             for idx, prio in zip(batch_indices, batch_priorities):
                 self.priorities[idx] = prio
 
+        # 借助权重计算loss
+        def calc_loss(batch, batch_weights, net, tgt_net, gamma, device="cpu"):
+            states, actions, rewards, dones, next_states = common.unpack_batch(batch)
+
+            states_v = torch.tensor(states).to(device)
+            next_states_v = torch.tensor(next_states).to(device)
+            actions_v =torch.tensor(actions).to(device)
+            rewards_v = torch.tensor(rewards).to(device)
+            done_mask = torch.ByteTensor(dones).to(device)
+            batch_weights_v = torch.tensor(batch_weights).to(device)
+
+            state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+            next_state_values = tgt_net(next_states_v).max(1)[0]
+            next_state_values[done_mask] = 0.0
+
+            expected_state_action_values = next_state_values.detach() * gamma +rewards_v
+            # 自实现的考虑到样本权重的损失函数
+            losses_v = batch_weights_v * (state_action_values - expected_state_action_values) ** 2
+            # 防止优先度为0的情况发生
+            return losses_v.mean(), losses_v + 1e-5
 
 
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
 
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
 
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-prio-replay")
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(net, selector, device=device)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    # 使用优先度的re[lay buffer而不是普通的
+    buffer = PrioReplayBuffer(exp_source, params['replay_size'], PRIO_REPLAY_ALPHA)
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    # 准备训练循环
+    frame_idx = 0
+    beta = BETA_START
+
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+            epsilon_tracker.frame(frame_idx)
+            # 递增的β策略
+            beta = min(1.0, BETA_START + frame_idx * (1.0 - BETA_START) / BETA_FRAMES)
+
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                writer.add_scalar("beta", beta, frame_idx)
+                if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
+                    break
+            if len(buffer) < params['replay_init']:
+                continue
+            optimizer.zero_grad()
+            # 返回三样东西
+            batch, batch_indices, batch_weights = buffer.sample(params['batch_size', beta])
+            loss_v, sample_prios_v = calc_loss(batch, batch_weights, net, tgt_net.target_model, params['gamma'], device=device)
+            loss_v.backward()
+            optimizer.step()
+            # 更新样本权重
+            buffer.update_priorities(batch_indices, sample_prios_v.data.cpu().numpy())
+
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
 
 
 
