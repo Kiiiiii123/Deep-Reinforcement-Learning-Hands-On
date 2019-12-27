329d865a9f84538bdaac20f7d380820e7f42ba12
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-27 22:09:32 +0800

    Update Chapter07/07_dqn_distrib.py

--- Chapter07/07_dqn_distrib.py
+++ Chapter07/07_dqn_distrib.py
@@ -54,6 +54,7 @@ class DistributionalDQN(nn.Module):
         )
 
         # 注册不应被视为模型参数的缓冲区，_buffer中的元素不会被优化器更新，如果在模型中需要需要一些参数，并且要通过state_dict返回，且不需要被优化器训练，那么这些参数可以注册在_buffer中
+        # torch.arange函数不包含末尾的按照指定间隔生成列表
         self.register_buffer("supports", torch.arange(Vmin, Vmax+DELTA_Z, DELTA_Z))
         # 对n维输入张量运用Softmax函数，将张量的每个元素缩放到（0,1）区间且和为1，dim=1表示按行计算
         self.softmax = nn.Softmax(dim=1)
@@ -70,12 +71,38 @@ class DistributionalDQN(nn.Module):
         fc_out = self.fc(conv_out)
         return fc_out.view(batch_size, -1, N_ATOMS)
 
+    # 从概率分布中获取Q-values
     def both(self, x):
         cat_out = self(x)
         probs = self.apply_softmax(cat_out)
         weights = probs * self.supports
         res = weights.sum(dim=2)
         return cat_out, res
+
+    # 计算Q-values
+    def qvals(self, x):
+        return self.both(x)[1]
+
+    # 对输出应用softmax函数并保持张量形式
+    def apply_softmax(self, t):
+        return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
+
+
+def calc_loss(batch, net, tgt_net, gamma, device="cpu", save_prefix=None):
+    states, actions, rewards, dones, next_states = common.unpack_batch(batch)
+    batch_size = len(batch)
+
+    states_v = torch.tensor(states).to(device)
+    actions = torch.tensor(actions).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+
+    # next state distribution
+    next_distr_v, next_qvals_v = tgt_net.both(next_states_v)
+    next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
+    next_distr = tgt_net.apply_softmax(next_distr_v).data.cpu().numpy()
+
+    next_best_distr = next_distr[range(batch_size), next_actions]
+    dones = dones.astype(np.bool)
 
 
 
