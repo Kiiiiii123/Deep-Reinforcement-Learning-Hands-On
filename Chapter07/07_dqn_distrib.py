8070d63a4e7a64d37e215920494ada4b83a00087
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-27 21:24:04 +0800

    Update Chapter07/07_dqn_distrib.py

--- Chapter07/07_dqn_distrib.py
+++ Chapter07/07_dqn_distrib.py
@@ -49,11 +49,33 @@ class DistributionalDQN(nn.Module):
         self.fc = nn.Sequential(
             nn.Linear(conv_out_size, 512),
             nn.ReLU(),
+            # 全连接层输出部分变为包含n_actions*N_ATOMS个元素的矩阵，包含每个动作额概率分布
             nn.Linear(512, n_actions * N_ATOMS)
         )
 
+        # 注册不应被视为模型参数的缓冲区，_buffer中的元素不会被优化器更新，如果在模型中需要需要一些参数，并且要通过state_dict返回，且不需要被优化器训练，那么这些参数可以注册在_buffer中
         self.register_buffer("supports", torch.arange(Vmin, Vmax+DELTA_Z, DELTA_Z))
+        # 对n维输入张量运用Softmax函数，将张量的每个元素缩放到（0,1）区间且和为1，dim=1表示按行计算
         self.softmax = nn.Softmax(dim=1)
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        fx = x.float() / 256
+        fx = fx.type(torch.FloatTensor).cuda()
+        conv_out = self.conv(fx).view(batch_size, -1)
+        fc_out = self.fc(conv_out)
+        return fc_out.view(batch_size, -1, N_ATOMS)
+
+    def both(self, x):
+        cat_out = self(x)
+        probs = self.apply_softmax(cat_out)
+        weights = probs * self.supports
+        res = weights.sum(dim=2)
+        return cat_out, res
 
 
 
