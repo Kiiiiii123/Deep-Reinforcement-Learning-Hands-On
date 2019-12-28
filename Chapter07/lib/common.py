d19810fa3f39b99709bb0280aaeb2ffb25349519
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-28 22:18:00 +0800

    Update Chapter07/lib/common.py

--- Chapter07/lib/common.py
+++ Chapter07/lib/common.py
@@ -133,24 +133,25 @@ def distr_projection(next_distr, rewards, dones, Vmin, Vmax, n_atoms, gamma):
         proj_distr[ne_mask, l[ne_mask]] += next_distr[ne_mask, atom] * (u - b_j)[ne_mask]
         proj_distr[ne_mask, u[ne_mask]] += next_distr[ne_mask, atom] * (b_j - l)[ne_mask]
 
-        if dones.any():
-            # 已经来到epside末尾的样例将分布置为0
-            proj_distr[dones] = 0.0
-            tz_j = np.minimum(Vmax, np.maximum(Vmin, rewards[dones]))
-            b_j = (tz_j - Vmin) / delta_z
-            l = np.floor(b_j).astype(np.int64)
-            u = np.ceil(b_j).astype(np.int64)
-            eq_mask = u == l
-            eq_dones = dones.copy()
-            eq_dones[dones] = eq_mask
-            if eq_dones.any():
-                proj_distr[eq_dones, l[eq_mask]] = 1.0
-            ne_mask = u != l
-            ne_dones = dones.copy()
-            ne_dones[dones] = ne_mask
-            if ne_dones.any():
-                proj_distr[ne_dones, l[ne_mask]] = (u - b_j)[ne_mask]
-                proj_distr[ne_dones, u[ne_mask]] = (b_j - l)[ne_mask]
+    if dones.any():
+        # 已经来到epside末尾的样例将分布置为0
+        proj_distr[dones] = 0.0
+        tz_j = np.minimum(Vmax, np.maximum(Vmin, rewards[dones]))
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+        eq_mask = u == l
+        eq_dones = dones.copy()
+        eq_dones[dones] = eq_mask
+        # 当可迭代对象中有任意一个不为False，则返回True
+        if eq_dones.any():
+            proj_distr[eq_dones, l[eq_mask]] = 1.0
+        ne_mask = u != l
+        ne_dones = dones.copy()
+        ne_dones[dones] = ne_mask
+        if ne_dones.any():
+            proj_distr[ne_dones, l[ne_mask]] = (u - b_j)[ne_mask]
+            proj_distr[ne_dones, u[ne_mask]] = (b_j - l)[ne_mask]
     return proj_distr
 
 
