22f89928ad06d41ff525728bd59e92884b29e8d7
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-28 23:59:38 +0800

    Update Chapter0707_dqn_distrib.py

--- Chapter07/07_dqn_distrib.py
+++ Chapter07/07_dqn_distrib.py
@@ -90,6 +90,59 @@ class DistributionalDQN(nn.Module):
         return self.both(x)[1]
 
 
+def calc_values_of_states(states, net, device="cpu"):
+    mean_vals = []
+    for batch in np.array_split(states, 64):
+        states_v = torch.tensor(batch).to(device)
+        action_values_v = net.qvals(states_v)
+        best_action_values_v = action_values_v.max(1)[0]
+        mean_vals.append(best_action_values_v.mean().item())
+    return np.mean(mean_vals)
+
+
+# 保存状态概率分布图
+def save_state_images(frame_idx, states, net, device="cpu", max_states=200):
+    ofs = 0
+    p = np.arange(Vmin, Vmax + DELTA_Z, DELTA_Z)
+    for batch in np.array_split(states, 64):
+        states_v = torch.tensor(batch).to(device)
+        action_prob = net.apply_softmax(net(states_v)).data.cpu().numpy()
+        batch_size, num_actions, _ = action_prob.shape
+        for batch_idx in range(batch_size):
+            plt.clf()
+            for action_idx in range(num_actions):
+                plt.subplot(num_actions, 1, action_idx+1)
+                plt.bar(p, action_prob[batch_idx, action_idx], width=0.5)
+            plt.savefig("states/%05d_%08d.png" % (ofs + batch_idx, frame_idx))
+        ofs += batch_size
+        if ofs >= max_states:
+            break
+
+
+# 保存概率分布投影效果
+def save_transition_images(batch_size, predicted, projected, next_distr, dones, rewards, save_prefix):
+    for batch_idx in range(batch_size):
+        is_done = dones[batch_idx]
+        reward = rewards[batch_idx]
+        plt.clf()
+        p = np.arange(Vmin, Vmax + DELTA_Z, DELTA_Z)
+        plt.subplot(3, 1, 1)
+        plt.bar(p, predicted[batch_idx], width=0.5)
+        plt.title("Predicted")
+        plt.subplot(3, 1, 2)
+        plt.bar(p, projected[batch_idx], width=0.5)
+        plt.title("Projected")
+        plt.subplot(3, 1, 3)
+        plt.bar(p, next_distr[batch_idx], width=0.5)
+        plt.title("Next state")
+        suffix = ""
+        if reward != 0.0:
+            suffix = suffix + "_%.0f" % reward
+        if is_done:
+            suffix = suffix + "_done"
+        plt.savefig("%s_%02d%s.png" % (save_prefix, batch_idx, suffix))
+
+
 def calc_loss(batch, net, tgt_net, gamma, device="cpu", save_prefix=None):
     states, actions, rewards, dones, next_states = common.unpack_batch(batch)
     batch_size = len(batch)
@@ -117,10 +170,93 @@ def calc_loss(batch, net, tgt_net, gamma, device="cpu", save_prefix=None):
     state_action_values = distr_v[range(batch_size), actions_v.data]
     state_log_sm_v = F.log_softmax(state_action_values, dim=1)
     proj_distr_v = torch.tensor(proj_distr).to(device)
+
+    # 每一帧保存状态的概率分布投影过程
+    if save_prefix is not None:
+        pred = F.softmax(state_action_values, dim=1).data.cpu().numpy()
+        save_transition_images(batch_size, pred, proj_distr, next_best_distr, dones, rewards, save_prefix)
+
     loss_v = -state_log_sm_v * proj_distr_v
     return loss_v.sum(dim=1).mean()
 
 
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+#    params['epsilon_frames'] *= 2
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-distrib")
+    net = DistributionalDQN(env.observation_space.shape, env.action_space.n).to(device)
+
+    tgt_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    # 智能体计算Q-values的方式变了，因此需要改动
+    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), selector, device=device)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    frame_idx = 0
+    # 取出用于评估的状态集
+    eval_states = None
+    prev_save = 0
+    save_prefix = None
+
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+            epsilon_tracker.frame(frame_idx)
+
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
+                    break
+
+            if len(buffer) < params['replay_initial']:
+                continue
+
+            if eval_states is None:
+                eval_states = buffer.sample(STATES_TO_EVALUATE)
+                eval_states = [np.array(transition.state, copy=False) for transition in eval_states]
+                eval_states = np.array(eval_states, copy=False)
+
+            optimizer.zero_grad()
+            batch = buffer.sample(params['batch_size'])
+
+            # 文件保存前缀
+            save_prefix = None
+            # 是否保存概率分布投影过程
+            if SAVE_TRANSITIONS_IMG:
+                interesting = any(map(lambda s: s.last_state is None or s.reward != 0.0, batch))
+                if interesting and frame_idx // 30000 > prev_save:
+                    save_prefix = "images/img_%08d" % frame_idx
+                    prev_save = frame_idx // 30000
+
+            loss_v = calc_loss(batch, net, tgt_net.target_model, gamma=params['gamma'], device=device, save_prefix=save_prefix)
+            loss_v.backward()
+            optimizer.step()
+
+            # 目标网络同步
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
+
+            # 状态评估
+            if frame_idx % EVAL_EVERY_FRAME == 0:
+                mean_val = calc_values_of_states(eval_states, net, device=device)
+                writer.add_scalar("values_mean", mean_val, frame_idx)
+
+            # 每一万帧保存一次状态概率分布图
+            if SAVE_STATES_IMG and frame_idx % 10000 == 0:
+                save_state_images(frame_idx, eval_states, net, device=device)
 
 
 
