772cb0ba7da0815be4fd0934575fb21891b3f443
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-30 00:29:22 +0800

    Update Chapter07/02_cartpole_reinforce.py

--- Chapter09/02_cartpole_reinforce.py
+++ Chapter09/02_cartpole_reinforce.py
@@ -47,5 +47,26 @@ if __name__ == "__main__":
     net = PGN(env.observation_space.shape[0], env.action_space.n)
     print(net)
 
-    # 使用全新的agent接口，对输出使用softmax将输出转换为概率
+    # 使用全新的agent接口，对输出使用softmax将输出转换为概率，每观察一次输出一次动作概率，随机采样选择动作
     agent = ptan.agent.PolicyAgent(net, preprocessor=ptan.agent.float32_preprocessor, apply_softmax=True)
+
+    exp_source = ptan.experience.ExperienceFirstLast(env, agent, gamma=GAMMA)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    # 记录整体情况
+    total_rewards = []
+    step_idx = 0
+    done_episodes = 0
+
+    # 抓取样本训练数据的变量
+    batch_episodes = 0
+    cur_rewards = []
+    batch_states, batch_actions, batch_qvals = [], [], []
+
+    for step_idx, exp in enumerate(exp_source):
+        batch_states.append(exp.state)
+        batch_actions.append(int(exp.action))
+        cur_rewards.append(exp.reward)
+
+
