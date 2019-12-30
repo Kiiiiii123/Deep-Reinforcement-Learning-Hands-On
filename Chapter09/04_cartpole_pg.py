274108d5c93fda452adc64bfcd2e93e451c8a833
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-30 22:51:10 +0800

    Update Chapter09/04_cartpole_pg.py

--- Chapter09/04_cartpole_pg.py
+++ Chapter09/04_cartpole_pg.py
@@ -32,3 +32,79 @@ class PGN(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    writer = SummaryWriter(comment="-cartpole-pg")
+
+    net = PGN(env.observation_space.shape[0], env.action_space.n)
+    print(net)
+
+    agent = ptan.agent.PolicyAgent(net, preprocessor=ptan.agent.float32_preprocessor, apply_softmax=True)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    total_rewards = []
+    step_rewards = []
+    step_idx = 0
+    done_episodes = 0
+    reward_sum = 0.0
+
+    batch_states, batch_actions, batch_scales = [], [], []
+
+    for step_idx, exp in enumerate(exp_source):
+        # 每个样本的折扣奖励之和
+        reward_sum += exp.reward
+        baseline = reward_sum / (step_idx + 1)
+        writer.add_scalar("baseline", baseline, step_idx)
+        batch_states.append(exp.state)
+        batch_actions.append(exp.action)
+        # 降低梯度尺寸的方差
+        batch_scales.append(exp.reward-baseline)
+
+        new_rewards = exp_source.pop_total_rewards()
+        if new_rewards:
+            done_episodes += 1
+            reward = new_rewards[0]
+            total_rewards.append(reward)
+            mean_rewards = float(np.mean(total_rewards)[-100:])
+            print("%d: reward: %6.2f, mean_100: %6.2f, episodes: %d"% (step_idx, reward, mean_rewards, done_episodes))
+            writer.add_scalar("reward", reward, step_idx)
+            writer.add_scalar("reward_100", mean_rewards, step_idx)
+            writer.add_scalar("episodes", done_episodes, step_idx)
+
+            if mean_rewards > 195:
+                print("Solved in %d steps and %d episodes"% (step_idx, done_episodes))
+                break
+
+        if len(batch_states) < BATCH_SIZE:
+            continue
+
+        states_v = torch.FloatTensor(batch_states)
+        batch_actions_t = torch.FloatTensor(batch_actions)
+        batch_scales_v = torch.FloatTensor(batch_scales)
+
+        # 求policy loss
+        optimizer.zero_grad()
+        logits_v = net(states_v)
+        log_prob_v = F.log_softmax(logits_v, dim=1)
+        log_prob_actions_v = batch_scales_v * log_prob_v[range(BATCH_SIZE), batch_actions_t]
+        loss_policy_v = -log_prob_actions_v.mean()
+
+        # 计算batch的熵作为熵奖金，从loss中减去
+        prob_v = F.softmax(logits_v, dim=1)
+        entropy_v = -(prob_v * log_prob_v).sum(dim=1).mean()
+        entropy_loss_v = -ENTROPY_BETA * entropy_v
+        loss_v = loss_policy_v + entropy_loss_v
+
+        loss_v.backward()
+        optimizer.step()
+
+        # 计算KL散度比较先前策略与优化一步后的策略上的变化
+
+
+
+
+
+
