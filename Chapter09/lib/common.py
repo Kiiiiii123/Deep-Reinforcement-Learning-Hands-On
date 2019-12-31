ce9738f1353fb0ffd39e913c4459b6f25ea8804a
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2019-12-31 21:05:31 +0800

    Update Chapter09/lib/common.py

--- Chapter09/lib/common.py
+++ Chapter09/lib/common.py
@@ -43,3 +43,32 @@ class RewardTracker:
 
 
 class AtariPGN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(AtariPGN, self).__init__()
+
+        self.conv = nn.Sequential(
+            # input_shape[0]为图像通道数
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_size(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def _get_conv_size(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        fx = fx.type(torch.FloatTensor).cuda()
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.fc(conv_out)
