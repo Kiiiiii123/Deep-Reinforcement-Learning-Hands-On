07a3244370bc94e0fd175f5ea8f89cd3d5ce75b4
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-02 22:56:20 +0800

    Update Chapter10/02_pong_a2c.py

--- Chapter10/02_pong_a2c.py
+++ Chapter10/02_pong_a2c.py
@@ -90,3 +90,49 @@ def unpack_batch(batch, net, device='cpu'):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    # 环境
+    make_env = lambda: ptan.common.wrappers.wrap_dqn(gym.make("PongNoFrameskip-v4"))
+    envs = [make_env() for _ in range(NUM_ENVS)]
+    writer = SummaryWriter(comment="-pong-a2c" + args.name)
+
+    # 网络
+    net = AtariA2C(envs[0].observation_space.shape, envs[0].action_space.n).to(device)
+    print(net)
+
+    # 智能体
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], apply_softmax=True, device=device)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
+
+    # 设定epsilon值较大以防止梯度过大影响训练的稳定性
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
+
+    batch = []
+
+    with common.RewardTracker(writer, stop_reward=18) as tracker:
+        # 新增的第二个wrapper，使得输入到tensorboard上的数据变得平滑
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                batch.append(exp)
+
+                # handle new rewards
+                new_rewards = exp_source.pop_total_rewards()
+                if new_rewards:
+                    if tracker.reward(new_rewards[0], step_idx):
+                        break
+
+                # 积累128个transitions才开始进行训练
+                if len(batch) < BATCH_SIZE:
+                    continue
+
+                states_v, actions_t, vals_ref_v = unpack_batch(batch, net, device=device)
+
+
+
+
+
