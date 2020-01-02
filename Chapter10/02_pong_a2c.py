ffb2e77beb61112c923f2c6289988b324ce4d236
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-02 16:52:38 +0800

    Update Chapter10/02_pong_a2c.py

--- Chapter10/02_pong_a2c.py
+++ Chapter10/02_pong_a2c.py
@@ -11,3 +11,53 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 from lib import common
+
+GAMMA = 0.99
+LEARNING_RATE = 0.001
+ENTROPY_BETA = 0.01
+BATCH_SIZE = 128
+NUM_ENVS = 50
+
+REWARD_STEPS = 4
+CLIP_GRAD = 0.1
+
+
+class AtariA2C(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(AtariA2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        # 第一个输出策略的神经网络
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        fx = fx.type(torch.FloatTensor).cuda()
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
+
+
