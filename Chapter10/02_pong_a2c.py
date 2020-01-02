94a1e4ed0f52b1392f40ae077269297495510a8c
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-02 17:50:38 +0800

    Update Chapter10/02_pong_a2c.py

--- Chapter10/02_pong_a2c.py
+++ Chapter10/02_pong_a2c.py
@@ -60,4 +60,33 @@ class AtariA2C(nn.Module):
         return self.policy(conv_out), self.value(conv_out)
 
 
+def unpack_batch(batch, net, device='cpu'):
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
 
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+        states_v = torch.FloatTensor(states).to(device)
+        actions_t = torch.LongTensor(actions).to(device)
+
+        # handle rewards
+        rewards_np = np.array(rewards, dtype=np.float32)
+        if not_done_idx:
+            last_states_v = torch.FloatTensor(last_states).to(device)
+            last_vals_v = net(last_states_v)[1]
+            last_vals_np = last_vals_v.data.cpu().numpy()[:,0]
+            rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
+
+        ref_vals_v = torch.FloatTensor(rewards_np).to(device)
+        return states_v, actions_t, ref_vals_v
+
+
+if __name__ == "__main__":
