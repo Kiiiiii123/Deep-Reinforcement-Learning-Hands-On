464a079f7952d9bed8eeb2dcf54e5df2fe193db4
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-03 10:31:05 +0800

    Update Chapter10/02_pong_a2c.py

--- Chapter10/02_pong_a2c.py
+++ Chapter10/02_pong_a2c.py
@@ -70,6 +70,7 @@ def unpack_batch(batch, net, device='cpu'):
     for idx, exp in enumerate(batch):
         states.append(np.array(exp.state, copy=False))
         actions.append(int(exp.action))
+        # 注意这里的reward不是即时奖励而是已经处理过后的折扣奖励，只有在该transition的N步中不存在结束episode才可以进行后续计算Q值的近似
         rewards.append(exp.reward)
         if exp.last_state is not None:
             not_done_idx.append(idx)
@@ -77,12 +78,13 @@ def unpack_batch(batch, net, device='cpu'):
         states_v = torch.FloatTensor(states).to(device)
         actions_t = torch.LongTensor(actions).to(device)
 
-        # handle rewards
+        # handle rewards，求出目标值ref_vals_v用于计算loss
         rewards_np = np.array(rewards, dtype=np.float32)
         if not_done_idx:
             last_states_v = torch.FloatTensor(last_states).to(device)
+            # 网络输出V(s)的近似
             last_vals_v = net(last_states_v)[1]
-            last_vals_np = last_vals_v.data.cpu().numpy()[:,0]
+            last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
             rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
 
         ref_vals_v = torch.FloatTensor(rewards_np).to(device)
@@ -131,8 +133,33 @@ if __name__ == "__main__":
                     continue
 
                 states_v, actions_t, vals_ref_v = unpack_batch(batch, net, device=device)
+                batch.clear()
 
+                optimizer.zero_grad()
+                logits_v, value_v = net(states_v)
+                # 输出single number网络的loss
+                loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
 
+                log_prob_v = F.log_softmax(logits_v, dim=1)
+                adv_v = vals_ref_v - value_v.detach()
+                log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                loss_policy_v = -log_prob_actions_v.mean()
+
+                prob_v = F.softmax(logits_v, dim=1)
+                entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+
+                # 只计算策略梯度
+                loss_policy_v.backward(retain_graph=True)
+                grads = np.concatenate([p.grad.data.cpu().numpy().flatten() for p in net.parameters() if p.grad is not None])
+
+                loss_v = entropy_loss_v + loss_value_v
+                loss_v.backward()
+                nn.utils.clip_grad_norm_(net.parameters(), CLIP_GRAD)
+                optimizer.step()
+                loss_v += loss_policy_v
+
+                tb_tracker.track("advantage", adv_v, step_idx)
+                tb_tracker.track("values", value_v, step_idx)
 
 
 
