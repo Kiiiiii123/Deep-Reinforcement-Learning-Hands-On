7e461239878e97619544bc9bffd1ee5c76b0b3b1
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-06 21:24:07 +0800

    Update Chapter11/01_a3c_data.py

--- Chapter11/01_a3c_data.py
+++ Chapter11/01_a3c_data.py
@@ -38,11 +38,31 @@ def make_env():
 TotalReward = collections.namedtuple('TotalReward', field_names='reward')
 
 
-# 在子进程中执行（与抓取、发送样本相关）
+# 在子进程中执行（与抓取、发送样本相关），queue被用来将数据从子进程传送到主进程中，采用多生产者单消费者模式，可以包含两种不同的对象
 def data_func(net, device, train_queue):
     envs = [make_env() for _ in range(NUM_ENVS)]
     agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], device = device, apply_softmax=True)
     exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_BOUND)
+
+    for exp in exp_source:
+        new_rewards = exp_source.pop_total_rewards()
+        if new_rewards:
+            train_queue.put(TotalReward(reward=np.mean(new_rewards)))
+        train_queue.put(exp)
+
+
+if __name__ == "__main__":
+    # 开启子进程的方法
+    mp.set_start_method('spawn')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action='store_true', help='Enable cuda')
+    parser.add_argument("-n", '--name', required=True, help='Name of the run')
+    args = parser.parse_args()
+    device = 'cuda' if args.cuda else 'cpu'
+    writer = SummaryWriter(comment='-a3c-data' + NAME + "_" +args.name)
+
+
+
 
 
 
