1546025a7911253c99a8457b68bde150dbc0d4a7
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-06 22:47:57 +0800

    Update Chapter11/01_a3c_data.py

--- Chapter11/01_a3c_data.py
+++ Chapter11/01_a3c_data.py
@@ -7,7 +7,7 @@ import collections
 from tensorboardX import SummaryWriter
 
 import torch.nn.utils as nn_utils
-import torch.functional as F
+import torch.nn.functional as F
 import torch.optim as optim
 # Pytorch对多进程的封装
 import torch.multiprocessing as mp
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         with common.RewardTracker(writer, stop_reward=REWARD_BOUND) as tracker:
             with ptan.common.utils.TBMeanTracker(writer, batch_size=100) as tb_tracker:
                 while True:
-                    # 从队列获取并处理可能的TotalReward对象
+                    # 从队列获取并处理TotalReward对象和experience
                     train_entry = train_queue.get()
                     if isinstance(train_entry, TotalReward):
                         if tracker.reward(train_entry.reward, step_idx):
@@ -96,11 +96,39 @@ if __name__ == "__main__":
                     if len(batch) < BATCH_SIZE:
                         continue
 
-                    states_v, actions_v, vals_ref_v = common.unpack_batch(batch, net, last_val_gamma = GAMMA ** REWARD_STEPS, device=device)
+                    states_v, actions_t, vals_ref_v = common.unpack_batch(batch, net, last_val_gamma = GAMMA ** REWARD_STEPS, device=device)
                     batch.clear()
 
                     optimizer.zero_grad()
+                    logits_v, value_v= net(states_v)
+                    loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
 
+                    log_prob_v = F.log_softmax(logits_v, dim=1)
+                    adv_v = vals_ref_v - value_v.detach()
+                    log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                    loss_policy_v = -log_prob_actions_v.mean()
+
+                    prob_v = F.softmax(logits_v, dim=1)
+                    entropy_loss_v = ENTROPY_BETA*(prob_v * log_prob_v).sum(dim=1).mean()
+
+                    loss_v = entropy_loss_v + loss_policy_v + loss_value_v
+                    loss_v.backward()
+                    nn_utils.clip_grad_norm_(net.parameters(), CLIP_GRAD)
+                    optimizer.step()
+
+                    tb_tracker.track("advantage", adv_v, step_idx)
+                    tb_tracker.track("values", value_v, step_idx)
+                    tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+                    tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+                    tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+                    tb_tracker.track("loss_value", loss_value_v, step_idx)
+                    tb_tracker.track("loss_total", loss_v, step_idx)
+    finally:
+        # 程序中断或问题解决时执行
+        for p in data_proc_list:
+            # 结束运行子进程并等待
+            p.terminate()
+            p.join()
 
 
 
