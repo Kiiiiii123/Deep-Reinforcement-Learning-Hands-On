5891a49dcaf2a3fb7978dc8d242baaa662ee8188
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-19 21:06:41 +0800

    Update Chapter11/02_a3c_grad.py

--- Chapter11/02_a3c_grad.py
+++ Chapter11/02_a3c_grad.py
@@ -6,4 +6,23 @@ from tensorboardX import SummaryWriter
 import torch
 import torch.nn.utils as nn_utils
 import torch.nn.functional as F
+import torch.optim as optim
+import torch.multiprocessing as mp
+
+from lib import common
+
+GAMMA = 0.99
+LEARNING_RATE = 0.001
+ENTROPY_BETA = 0.01
+
+REWARD_STEPS = 4
+CLIP_GRAD = 0.1
+
+PROCESSES_COUNT = 4
+NUM_ENVS = 15
+
+GRAD_BATCH = 64
+TRAIN_BATCH = 2
+
+
 
