e288a4d69eea104ac38dd1b0858540d95070d0fb
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-19 21:34:15 +0800

    Update Chapter11/02_a3c_grad.py

--- Chapter11/02_a3c_grad.py
+++ Chapter11/02_a3c_grad.py
@@ -21,8 +21,13 @@ CLIP_GRAD = 0.1
 PROCESSES_COUNT = 4
 NUM_ENVS = 15
 
+# BATCH_SIZE用下面两个超参数进行替代
+# 定义每个子进程进行loss和gradient计算的batch大小
 GRAD_BATCH = 64
+# 在单个GPU上进行测试时的选择2，是每次中心进程进行SGD迭代时从子进程获取的gradients batch
 TRAIN_BATCH = 2
+# 每个优化步骤需要GRAD_BATCH * TRAIN_BATCH个样本
+
 ENV_NAME = 'PongNoFrameskip-v4'
 NAME = 'pong'
 REWARD_BOUND = 18
