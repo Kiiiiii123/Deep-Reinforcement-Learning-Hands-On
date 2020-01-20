a1cee1eb75a65120e08fb6e959cfbf3cd026a600
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-20 21:32:17 +0800

    Update Chapter14/lib/model.py

---
+++ Chapter14/lib/model.py
@@ -0,0 +1,34 @@
+import ptan
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+HID_SIZE = 128
+
+
+class ModelA2C(nn.Module):
+    def __init__(self, ob_size, act_size):
+        super(ModelA2C, self).__init__()
+
+        self.base = nn.Sequential(
+            nn.Linear(ob_size, HID_SIZE),
+            nn.ReLU(),
+        )
+        self.mu = nn.Sequential(
+            nn.Linear(HID_SIZE, act_size),
+            nn.Tanh(),
+        )
+        self.var = nn.Sequential(
+            nn.Linear(HID_SIZE, act_size),
+            nn.Softplus(),
+        )
+        self.value = nn.Linear(HID_SIZE, 1)
+
+    def forward(self, x):
+        base_out = self.base(x)
+        return self.mu(base_out), self.var(base_out), self.value(base_out)
+
+class DDPGActor(nn.Module):
+
