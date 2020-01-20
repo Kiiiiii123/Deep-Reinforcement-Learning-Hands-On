78df0e1667cf1b5e1142adde8d3a73daf897e3c7
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-20 22:01:48 +0800

    Update Chapter14/lib/model.py

--- Chapter14/lib/model.py
+++ Chapter14/lib/model.py
@@ -30,5 +30,20 @@ class ModelA2C(nn.Module):
         base_out = self.base(x)
         return self.mu(base_out), self.var(base_out), self.value(base_out)
 
-class DDPGActor(nn.Module):
+
+class AgentA2C(ptan.agent.BaseAgent):
+    def __init__(self, net, device='cpu'):
+        self.net = net
+        self.device = device
+
+        def __call__(self, states, agent_states):
+            states_v = ptan.agent.float32_preprocessor(states).to(device)
+            mu_v, var_v, _= net(states_v)
+            mu = mu_v.cpu().numpy()
+            sigma = torch.sqrt(var_v).data.cpu().numpy()
+            actions = np.random.normal(mu, sigma)
+            actions = np.clip(actions, -1, 1)
+            return actions, agent_states
+
+
 
