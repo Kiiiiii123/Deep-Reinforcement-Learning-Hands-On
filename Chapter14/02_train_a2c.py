a4d50eafcfe890aabfd06c5d62932b51cf497d0b
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-21 20:29:26 +0800

    Update Chapter14/02_train_a2c.py

--- Chapter14/02_train_a2c.py
+++ Chapter14/02_train_a2c.py
@@ -24,7 +24,7 @@ ENTROPY_BETA = 1e-4
 TEST_ITERS = 1000
 
 
-# 在测试环境下对网络模型进行测试，不进行随机采样
+# 在测试环境下对网络模型进行测试，不进行随机采样，网络直接用于多次推理计算均值
 def test_net(net, env, count=10, device='cpu'):
     rewards = 0.0
     steps = 0
@@ -64,4 +64,39 @@ if __name__ == '__main__':
 
     net = model.ModelA2C(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
     print(net)
+
+    writer = SummaryWriter(comment='-a2c_' + args.name)
+    agent = model.AgentA2C(net, device=device)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    batch = []
+    best_reward = None
+    with ptan.common.utils.RewardTracker(writer) as tracker:
+        # TensorBoard value tracker: allows to batch fixed amount of historical values and write their mean into TB
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                rewards_steps = exp_source.pop_rewards_steps()
+                if rewards_steps:
+                    rewards, steps = zip(*rewards_steps)
+                    tb_tracker.track('episode_steps', steps[0], step_idx)
+                    tracker.reward(rewards[0], step_idx)
+
+                if step_idx % TEST_ITERS == 0:
+                    ts = time.time()
+                    rewards, steps = test_net(net, test_env, device=device)
+                    print('Test done in %.2f sec, reward %.3f, steps %d' % (time.time() - ts, rewards, steps))
+                    writer.add_scalar('test_reward', rewards, step_idx)
+                    writer.add_scalar('test_steps', steps, step_idx)
+                    if best_reward is None or best_reward < rewards:
+                        if best_reward is not None:
+                            print('Best reward updated: %.3f -> %.3f' % (best_reward, rewards))
+                            name = 'best_%.3f_%d.dat' % (rewards, step_idx)
+                            fname = os.path.join(save_path, name)
+                            torch.save(net.state_dict(), fname)
+                        best_reward = rewards
+
+
+
 
