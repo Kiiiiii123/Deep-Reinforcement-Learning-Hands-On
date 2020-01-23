b675000ec30cebed1611a32173f66e2afe560e52
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-23 20:56:19 +0800

    Update Chapter14/04_train_ddpg.py

--- Chapter14/04_train_ddpg.py
+++ Chapter14/04_train_ddpg.py
@@ -24,3 +24,36 @@ REPLAY_INITIAL = 10000
 
 TEST_ITERS = 1000
 
+
+def test_net(net, env, count=10, device="cpu"):
+    rewards = 0.0
+    steps = 0
+    for _ in range(count):
+        obs = env.reset()
+        while True:
+            obs_v = ptan.agent.float32_preprocessor([obs]).to(device)
+            mu_v = net(obs_v)
+            action = mu_v.squeeze(dim=0).data.cpu().numpy()
+            action = np.clip(action, -1, 1)
+            obs, reward, done, _ = env.step(action)
+            rewards += reward
+            steps += 1
+            if done:
+                break
+    return rewards / count, steps / count
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action='store_true', help='Enable CUDA')
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    save_path = os.path.join('saves', 'ddpg' + args.name)
+    os.makedirs(save_path, exist_ok=True)
+
+    env = gym.make(ENV_ID)
+    test_env = gym.make(ENV_ID)
+
+
