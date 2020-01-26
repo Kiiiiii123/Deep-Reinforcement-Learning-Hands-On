9b9f19c729895347d09df6bcf76b2af88fc16ec7
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-01-26 22:06:39 +0800

    Update Chapter14/06_train_d4pg.py

--- Chapter14/06_train_d4pg.py
+++ Chapter14/06_train_d4pg.py
@@ -1,2 +1,16 @@
 # 较新的论文，相较于DDPG提升了训练的稳定性，收敛速度以及样本采样效率，将DQN拓展方法中的思想应用到DDPG中
+import os
+import ptan
+import time
+import gym
+import pybullet_envs
+import argparse
+from tensorboardX import SummaryWriter
+import numpy as np
+
+from lib import model, common
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
 
