438cc2daa2308970391ecbbb5e9f105c179ecbfe
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-24 22:27:13 +0800

    Update Chapter02/01_agent_anatomy.py

--- Chapter02/01_agent_anatomy.py
+++ Chapter02/01_agent_anatomy.py
@@ -1,43 +1,53 @@
+# Python type annotation
+from typing import List
 import random
 
 
 class Environment:
     def __init__(self):
+        # limit the number of time steps that the agent is allowed to take to interact with the environment
         self.steps_left = 10
 
-    def get_observation(self):
+    # return the current environment's observation to the agent
+    def get_observation(self) -> List[float]:
         return [0.0, 0.0, 0.0]
 
-    def get_actions(self):
+    # allow the agent to query the set of actions it can execute
+    def get_actions(self) -> List[int]:
         return [0, 1]
 
-    def is_done(self):
+    # signal the end of the episode to the agent
+    def is_done(self) -> bool:
         return self.steps_left == 0
 
-    def action(self, action):
+    # handle an agent's action and return the reward for this action.
+    def action(self, action: int) -> float:
         if self.is_done():
-            raise Exception("Game is over!")
+            raise Exception('Game is over!')
         self.steps_left -=1
         return random.random()
 
 
 class Agent:
     def __init__(self):
+        # initialize the counter that will keep the total reward accumulated by the agent during the episode
         self.total_reward = 0.0
 
-    def step(self, env):
+    # accept the environment instance as an argument and allow the agent to perform the actions
+    def step(self, env: Environment):
         current_obs = env.get_observation()
         actions = env.get_actions()
         reward = env.action(random.choice(actions))
         self.total_reward += reward
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     env = Environment()
     agent = Agent()
 
     while not env.is_done():
         agent.step(env)
 
-    print("Total reward got:%.4f"%agent.total_reward)
+    print('Total reward got: %.4f' % agent.total_reward)
+
 
