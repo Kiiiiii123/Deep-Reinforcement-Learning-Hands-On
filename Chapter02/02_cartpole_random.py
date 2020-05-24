b9475d6619b1996d048c4354c1f4a6e2c064b4b4
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-25 00:41:38 +0800

    Update Chapter02/02_cartpole_random.py

--- Chapter02/02_cartpole_random.py
+++ Chapter02/02_cartpole_random.py
@@ -1,6 +1,7 @@
 import gym
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     env = gym.make('CartPole-v0')
 
     total_reward = 0.0
@@ -11,8 +12,8 @@ if __name__ == "__main__":
         action = env.action_space.sample()
         obs, reward, done, _ = env.step(action)
         total_reward += reward
-        total_steps +=1
+        total_steps += 1
         if done:
             break
 
-    print('Episode done in %d steps,total reward %.2f'%(total_steps, total_reward))
+    print('Episode done in %d steps, total reward %.2f' % (total_steps, total_reward))
