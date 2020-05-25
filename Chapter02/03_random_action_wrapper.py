adbc0ec16eb0fbe210ce3704a881d79e181307de
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-25 16:56:11 +0800

    Update Chapter02/03_random_action_wrapper.py

--- Chapter02/03_random_action_wrapper.py
+++ Chapter02/03_random_action_wrapper.py
@@ -6,10 +6,12 @@ Action = TypeVar('Action')
 
 
 class RandomActionWrapper(gym.ActionWrapper):
+    # initialize our wrapper by calling a parent's __init__ method and saving epsilon
     def __init__(self, env, epsilon=0.1):
         super(RandomActionWrapper, self).__init__(env)
         self.epsilon = epsilon
 
+    # override from a parent's class to tweak the agent's actions
     def action(self, action: Action) -> Action:
         if random.random() < self.epsilon:
             print('Random!')
@@ -18,6 +20,7 @@ class RandomActionWrapper(gym.ActionWrapper):
 
 
 if __name__ == '__main__':
+    # create a normal CartPole environment and pass it to our Wrapper constructor
     env = RandomActionWrapper(gym.make('CartPole-v0'))
     total_reward = 0.0
     obs = env.reset()
