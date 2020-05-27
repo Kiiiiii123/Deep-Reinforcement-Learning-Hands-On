aa1b8886f5cafa33285fa0102e81f78dc9a4a7d8
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-27 23:11:24 +0800

    Update Chapter03/03_atari_gan.py

--- Chapter03/03_atari_gan.py
+++ Chapter03/03_atari_gan.py
@@ -107,6 +107,27 @@ class Generator(nn.Module):
         return self.deconv_pipe(x)
 
 
+def iterate_batches(envs, batch_size=BATCH_SIZE):
+    batch = [e.reset() for e in envs]
+    env_gen = iter(lambda: random.choice(envs), None)
+
+    while True:
+        e = next(env_gen)
+        obs, reward, is_done, _ = e.step(e.action_space.sample())
+        if np.mean(obs) > 0.01:
+            batch.append(obs)
+        if len(batch) == batch_size:
+            # Normalizing input between -1 to 1
+            batch_np = np.array(batch, dtype=np.float32)
+            batch_np *= 2.0/255.0 - 1.0
+            yield torch.tensor(batch_np)
+            batch.clear()
+        if is_done:
+            e.reset()
+
+
+if __name__ == '__main__':
+
 
 
 
