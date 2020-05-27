252ed2c81854b98c2f16b26c3b9c1448dab96278
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-27 21:25:51 +0800

    Update Chapter03/03_atari_gan.py

--- Chapter03/03_atari_gan.py
+++ Chapter03/03_atari_gan.py
@@ -37,6 +37,20 @@ class InputWrapper(gym.ObservationWrapper):
     """
     def __init__(self, *args):
         super(InputWrapper, self).__init__(*args)
+        assert isinstance(self.observation_space, gym.spaces.Box)
+        old_space = self.observation_space
+        self.observation_space = gym.spaces.Box(
+            self.observation(old_space.low),
+            self.observation(old_space.high),
+            dtype = np.float32
+        )
+
+    def observation(self, observation):
+        new_obs = cv2.resize(observation, (IMAGE_SIZE, IMAGE_SIZE))
+        # transform (210, 160,3) -> (3, 210, 160)
+        new_obs = np.moveaxis(new_obs, 2, 0)
+        return new_obs.astype(np.float32)
+
 
 
 
