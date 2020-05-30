59a33ca0ed57913ba28bf215405e49093107e9bd
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-30 23:55:27 +0800

    Update Chapter04/02_frozenlake_naive.py

--- Chapter04/03_frozenlake_tweaked.py
+++ Chapter04/03_frozenlake_tweaked.py
@@ -1,28 +1,27 @@
+import random
 import gym
+import gym.spaces
 from collections import namedtuple
 import numpy as np
 from tensorboardX import SummaryWriter
-import random
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
+
 HIDDEN_SIZE = 128
-# 每次迭代至少100次episode
 BATCH_SIZE = 100
 PERCENTILE = 30
-# 加入奖励折扣因子以衡量episode的好坏程度
 GAMMA = 0.9
 
 
-# 将数值输入更换为独热编码
 class DiscreteOneHotWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super(DiscreteOneHotWrapper, self).__init__(env)
-        # 断言，判断表达式为假是触发异常，插入调试断点到程序的一种便捷方式
         assert isinstance(env.observation_space, gym.spaces.Discrete)
-        self.observation_space = gym.spaces.Box(0.0, 1.0, (env.observation_space.n, ), dtype=np.float32)
+        shape = (env.observation_space.n, )
+        self.observation_space = gym.spaces.Box(0.0, 1.0, shape, dtype=np.float32)
 
     def observation(self, observation):
         res = np.copy(self.observation_space.low)
@@ -60,9 +59,11 @@ def iterate_batches(env, net, batch_size):
         action = np.random.choice(len(act_probs), p=act_probs)
         next_obs, reward, is_done, _ = env.step(action)
         episode_reward += reward
-        episode_steps.append(EpisodeStep(observation=obs, action=action))
+        episode_step = EpisodeStep(observation=obs, action=action)
+        episode_steps.append(episode_step)
         if is_done:
-            batch.append(Episode(reward=episode_reward, steps=episode_steps))
+            episode = Episode(reward=episode_reward, steps=episode_steps)
+            batch.append(episode)
             episode_reward = 0.0
             episode_steps = []
             next_obs = env.reset()
@@ -73,60 +74,57 @@ def iterate_batches(env, net, batch_size):
 
 
 def filter_batch(batch, percentile):
-    # 引入折扣因子
-    disc_rewards = list(map(lambda s: s.reward * (GAMMA ** len(s.steps)), batch))
+    disc_rewards = list(map(lambda episode: episode.reward * (GAMMA ** len(episode.steps)), batch))
     reward_bound = np.percentile(disc_rewards, percentile)
 
     train_obs = []
     train_act = []
-    # 存储本就不多的好的episode
     elite_batch = []
-    # zip()函数将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表
-    for example, discounted_reward in zip(batch, disc_rewards):
-        if discounted_reward > reward_bound:
-            train_obs.extend(map(lambda step: step.observation, example.steps))
-            train_act.extend(map(lambda step: step.action, example.steps))
-            elite_batch.append(example)
+    for episode, disc_reward in zip(batch, disc_rewards):
+        if disc_reward > reward_bound:
+            train_obs.extend(map(lambda step: step.observation, episode.steps))
+            train_act.extend(map(lambda step: step.action, episode.steps))
+            elite_batch.append(episode)
 
     return elite_batch, train_obs, train_act, reward_bound
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     random.seed(12345)
-    env = DiscreteOneHotWrapper(gym.make("FrozenLake-v0"))
-    # env = gym.wrappers.Monitor(env, directory="mon", force=True)
+    env = DiscreteOneHotWrapper(gym.make('FrozenLake-v0'))
     obs_size = env.observation_space.shape[0]
     n_actions = env.action_space.n
 
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
     objective = nn.CrossEntropyLoss()
-    # 降低学习率以平均更多训练样本
-    optimizer = optim.Adam(params=net.parameters(), lr=0.001)
-    writer = SummaryWriter(comment="-frozenlake-tweaked")
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    writer = SummaryWriter(comment='-frozenlake-tweaked')
 
     full_batch = []
-    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
-        reward_m = float(np.mean(list(map(lambda s:s.reward, batch))))
-        full_batch, obs, acts, reward_b = filter_batch(full_batch+batch, PERCENTILE)
+    for iter_num, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        reward_mean = float(np.mean(list(map(lambda episode: episode.reward, batch))))
+        full_batch, obs, act, reward_bound = filter_batch(full_batch + batch, PERCENTILE)
         if not full_batch:
             continue
         obs_v = torch.FloatTensor(obs)
-        acts_v = torch.LongTensor(acts)
-        # 长期保有100个episode
+        act_v = torch.LongTensor(act)
         full_batch = full_batch[-500:]
 
         optimizer.zero_grad()
-        action_scores_v = net(obs_v)
-        loss_v = objective(action_scores_v, acts_v)
+        act_score_v = net(obs_v)
+        loss_v = objective(act_score_v, act_v)
         loss_v.backward()
         optimizer.step()
-        print("%d: loss=%.3f, reward_mean=%.3f, reward_bound=%.3f, batch=%d" % (
-            iter_no, loss_v.item(), reward_m, reward_b, len(full_batch)))
-        writer.add_scalar("loss", loss_v.item(), iter_no)
-        writer.add_scalar("reward_bound", reward_b, iter_no)
-        writer.add_scalar("reward_mean", reward_m, iter_no)
-        # 修改训练结束条件
-        if reward_m > 0.8:
-            print("Solved!")
+        print('%d: loss=%.3f, reward_mean=%.3f, reward_bound=%.3f, batch=%d' %
+              (iter_num, loss_v.item(), reward_mean, reward_bound, len(full_batch)))
+        writer.add_scalar('loss', loss_v.item(), iter_num)
+        writer.add_scalar('reward_mean', reward_mean, iter_num)
+        writer.add_scalar('reward_bound', reward_bound, iter_num)
+        if reward_mean > 0.8:
+            print('Solved!')
             break
     writer.close()
+
+
+
+
