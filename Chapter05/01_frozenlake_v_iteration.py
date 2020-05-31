6b83e172eae8a9f929d9ed608517d01ab8adbb94
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-01 00:19:53 +0800

    Update Chapter05/01_frozenlake_v_iteration.py

--- Chapter05/01_frozenlake_v_iteration.py
+++ Chapter05/01_frozenlake_v_iteration.py
@@ -42,6 +42,49 @@ class Agent:
                 best_action = action
         return best_action
 
-    def play_episodes(self, count):
+    def play_episode(self, env):
+        total_reward = 0.0
+        state = env.reset()
+        while True:
+            action = self.select_action(state)
+            new_state, reward, is_done, _ = env.step(action)
+            self.rewards[(state, action, new_state)] = reward
+            self.transitions[(state, action)][new_state] += 1
+            total_reward += reward
+            state = new_state
+            if is_done:
+                break
+        return total_reward
+
+    def value_iteration(self):
+        for state in range(self.env.observation_space.n):
+            state_values = [self.calc_action_value(state, action) for action in range(self.env.action_space.n)]
+            self.values [state] = max(state_values)
+
+
+if __name__ == '__main__':
+    test_env = gym.make(ENV_NAME)
+    agent = Agent()
+    writer = SummaryWriter(comment='-v-iteration')
+
+    iter_num = 0
+    best_reward = 0.0
+    while True:
+        iter_num += 1
+        agent.play_n_random_steps(100)
+        agent.value_iteration()
+
+        reward = 0.0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(test_env)
+        reward /= TEST_EPISODES
+        writer.add_scalar('reward', reward, iter_num)
+        if reward > best_reward:
+            print('Best reward updated: %.3f -> %.3f' % (best_reward, reward))
+            best_reward = reward
+        if reward > 0.80:
+            print('Solved in %d iterations!' % iter_num)
+            break
+    writer.close()
 
 
