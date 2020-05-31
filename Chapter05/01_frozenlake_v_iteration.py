b26409202d9104d35a30994816815b0f77480912
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-05-31 23:33:55 +0800

    Update Chapter05/01_frozenlake_v_iteration.py

--- Chapter05/01_frozenlake_v_iteration.py
+++ Chapter05/01_frozenlake_v_iteration.py
@@ -1,5 +1,29 @@
+import gym
+import collections
+from tensorboardX import SummaryWriter
+
+ENV_NAME = 'FrozenLake-v0'
+GAMMA = 0.9
+TEST_EPISODES = 20
 
 
+class Agent:
+    def __init__(self):
+        self.env = gym.make(ENV_NAME)
+        self.state = self.env.reset()
+        self.rewards = collections.defaultdict(float)
+        self.transitions = collections.defaultdict(collections.Counter)
+        self.values = collections.defaultdict(float)
+
+    def play_n_random_steps(self, count):
+        for _ in range(count):
+            action = self.env.action_space.sample()
+            new_state, reward, is_done, _ = self.env.step(action)
+            self.rewards[(self.state, action, new_state)] = reward
+            self.transitions[(self.state, action)][new_state] += 1
+            self.state = self.env.reset() if is_done else new_state
+
+    def
 
 
 
