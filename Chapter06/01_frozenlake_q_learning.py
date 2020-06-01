e93f7d51e2064d50e20424d1c75aa9b6452a5cb0
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-01 21:47:28 +0800

    Update Chapter06/01_frozenlake_q_learning.py

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -24,6 +24,57 @@ class Agent:
     def best_value_and_action(self, state):
         best_value, best_action = None, None
         for action in range(self.env.action_space.n):
+            action_value = self.value_table[(state, action)]
+            if best_value is None or best_value < action_value:
+                best_value = action_value
+                best_action = action
+        return best_value, best_action
+
+    def value_table_update(self, s, a, r, next_s):
+        best_v, _ = self.best_value_and_action(next_s)
+        new_v = r + GAMMA * best_v
+        old_v = self.value_table[(s,a)]
+        self.value_table[(s, a)] = (1 - ALPHA) * old_v + ALPHA * new_v
+
+    def play_episode(self, env):
+        total_reward = 0.0
+        state = env.reset()
+        while True:
+            _, action = self.best_value_and_action(state)
+            new_state, reward, is_done, _ = env.step(action)
+            total_reward += reward
+            if is_done:
+                break
+            state = new_state
+        return total_reward
+
+
+if __name__ == '__main__':
+    test_env = gym.make(ENV_NAME)
+    agent = Agent()
+    writer = SummaryWriter(comment='-q-learning')
+
+    iter_num = 0
+    best_reward = 0.0
+    while True:
+        iter_num += 1
+        s, a, r, next_s = agent.get_sample()
+        agent.value_table_update(s, a, r, next_s)
+
+        reward = 0.0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(test_env)
+        reward /= TEST_EPISODES
+        writer.add_scalar('reward', reward, iter_num)
+        if reward > best_reward:
+            print('Best reward updated %.3f -> %.3f' % (best_reward, reward))
+            best_reward = reward
+        if reward > 0.80:
+            print('Solved in %d iterations!' % iter_num)
+            break
+    writer.close()
+
+
 
 
 
