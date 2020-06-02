55df7c5edf66bd0af29922fec646468498f1b87d
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-02 19:45:58 +0800

    Update Chapter06/lib/wrappers.py

--- Chapter06/lib/wrappers.py
+++ Chapter06/lib/wrappers.py
@@ -50,3 +50,28 @@ class MaxAndSkipEnv(gym.Wrapper):
         obs = self.env.reset()
         self._obs_buffer.append(obs)
         return obs
+
+
+class ProcessFrame84(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        """Convert input observations from the emulator to a grayscale 84*84 image"""
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+
+    def observation(self, observation):
+        return ProcessFrame84.process(observation)
+
+    @staticmethod
+    def process(self, frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
+        else:
+            assert False, 'Unknown solution.'
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
+
