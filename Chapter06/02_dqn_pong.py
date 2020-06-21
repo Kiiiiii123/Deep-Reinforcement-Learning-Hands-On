a37163440b18b567ca05f6b608a2dfa4c9252f4a
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-21 19:11:12 +0800

    Update Chapter06/02_dqn_pong.py

---
+++ Chapter06/02_dqn_pong.py
@@ -0,0 +1,15 @@
+from Chapter06.lib import wrappers
+from Chapter06.lib import dqn_model
+
+import argparse
+import time
+import numpy as np
+import collections
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+
+
