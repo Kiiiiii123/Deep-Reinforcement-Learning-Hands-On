57c227d7af8fae0d9a5be4adb090cb84a252f2dd
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-22 15:37:15 +0800

    Update Chapter06/02_dqn_pong.py

--- Chapter06/02_dqn_pong.py
+++ Chapter06/02_dqn_pong.py
@@ -13,3 +13,115 @@ import torch.optim as optim
 from tensorboardX import SummaryWriter
 
 
+DEFAULT_ENV_NAME = 'PongNoFrameSkip-v4'
+MEAN_REWARD_BOUND = 19.0
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+REPLAY_START_SIZE = 10000
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 1000
+
+EPSILON_DECAY_LAST_FRAME = 150000
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.01
+
+Experience = collections.namedtuple('Experience', field_names=['state', 'action', 'reward', 'done', 'new_state'])
+
+
+class ExperienceBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, new_states = zip(*[self.buffer[index] for index in indices])
+        return np.array(states), np.array(actions), np.array(rewards, np.float32), np.array(dones, np.uint8), np.array(new_states)
+
+
+class Agent:
+    def __init__(self, env, exp_buffer):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self._reset()
+
+    def _reset(self):
+        self.state = self.env.reset()
+        self.total_reward = 0.0
+
+    def play_step(self, net, epsilon=0.0, device='cpu'):
+        done_reward = None
+
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            state_a = np.array([self.state], copy=False)
+            state_v = torch.tensor(state_a).to(device)
+            q_vals_v = net(state_v)
+            _, act_v = torch.max(q_vals_v, dim=1)
+            action = int(act_v.item())
+
+        new_state, reward, is_done = self.env.step(action)
+        self.total_reward += reward
+
+        exp = ExperienceBuffer(self.state, action, reward, is_done, new_state)
+        self.exp_buffer.append(exp)
+        self.state = new_state
+        if is_done:
+            done_reward = self.total_reward
+        return done_reward
+
+
+def calc_loss(batch, net, tgt_net, device='cpu'):
+    states, actions, rewards, dones, new_states = batch
+
+    states_v = torch.tensor(np.array(states, copy=False)).to(device)
+    new_states_v = torch.tensor(np.array(new_states, copy=False)).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.BoolTensor(dones).to(device)
+
+    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).sequeeze(-1)
+    next_state_values = tgt_net(new_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+    next_state_values = next_state_values.detach()
+
+    expected_state_action_values = rewards_v + next_state_values * GAMMA
+    return nn.MSELoss(state_action_values, expected_state_action_values)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', default=False, action='store true', help='Enable cuda')
+    parser.add_argument('--env', default= DEFAULT_ENV_NAME, help='Name of the environment, default=' + DEFAULT_ENV_NAME)
+    args = parser.parse_args()
+    device = torch.device('cuda' if args.cuda else 'cpu')
+
+    env = wrappers.make_env(args.env)
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+
+    writer = SummaryWriter(comment='-' + args.env)
+    print(net)
+
+    replay_buffer = ExperienceBuffer(REPLAY_SIZE)
+    agent = Agent(env, replay_buffer)
+    epsilon = EPSILON_START
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_mean_reward = None
+
+
+
+
