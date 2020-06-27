1fdd155ae4ee1c8e95f3aee13fc539b613ceabbf
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-27 12:36:22 +0800

    Update Chapter19/01_train_a2c.py

--- Chapter19/01_train_a2c.py
+++ Chapter19/01_train_a2c.py
@@ -1,1 +1,16 @@
+import ptan
+import gym
 import roboschool
+import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+ENV_ID = 'RoboschoolHalfCheetah-v1'
+
+
+if __name__ == '__main__':
+
