16aa507d81f8d6361f0c9cc09f89ebf4c376c629
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-06-28 13:56:02 +0800

    Update Chapter19/01_train_a2c.py

--- Chapter19/lib/common.py
+++ Chapter19/lib/common.py
@@ -4,3 +4,22 @@ import torch
 
 
 def unpack_batch_a2c(batch, net, last_val_gamma, device='cpu'):
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in batch:
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(exp.last_state)
+    states_v = ptan.agent.float32_preprocessor(states).to(device)
+    actions_v = torch.FloatTensor(actions).to(device)
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+
